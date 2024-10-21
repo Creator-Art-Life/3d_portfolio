1416230da2a01b4988a319fc4c4d08cc48637bb8
Creator-Art-Life <maximshool12@gmail.com>
2024-10-21 21:46:59 +0300

    more edit

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -9,7 +9,7 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={1} groundColor='black' />
+      <hemisphereLight intensity={0.15} groundColor='black' />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
@@ -18,7 +18,8 @@ const Computers = ({ isMobile }) => {
         castShadow
         shadow-mapSize={1024}
       />
-      <pointLight intensity={3} />
+      <pointLight intensity={1.5} /> {/* Увеличиваем интенсивность */}
+      <ambientLight intensity={0.5} /> {/* Добавляем ambient light */}
       <primitive
         object={computer.scene}
         scale={isMobile ? 0.7 : 0.75}

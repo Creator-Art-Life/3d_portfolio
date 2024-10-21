95c52e82385658132d4adc52fa2b26d9e6d41d5a
Creator-Art-Life <maximshool12@gmail.com>
2024-10-21 21:39:58 +0300

    computer
    edit them

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,19 +5,20 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF('./desktop_pc/scene.gltf')
+  const computer = useGLTF("./desktop_pc/scene.gltf");
+
   return (
     <mesh>
-      <hemisphereLight intensity={2} groundColor='black' />
-      <pointLight intensity={7} />
+      <hemisphereLight intensity={0.15} groundColor='black' />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
         penumbra={1}
         intensity={1}
-        castShadow={1}
+        castShadow
         shadow-mapSize={1024}
       />
+      <pointLight intensity={1} />
       <primitive
         object={computer.scene}
         scale={isMobile ? 0.7 : 0.75}
@@ -25,29 +26,28 @@ const Computers = ({ isMobile }) => {
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
-  )
-}
-
+  );
+};
 
 const ComputersCanvas = () => {
-  const [isMobile, setIsMobile] = useState(false)
+  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    // Добавить прослушиватель для изменений размера экрана
+    // Add a listener for changes to the screen size
     const mediaQuery = window.matchMedia("(max-width: 500px)");
 
-    // Установить начальное значение переменной состояния `isMobile`
+    // Set the initial value of the `isMobile` state variable
     setIsMobile(mediaQuery.matches);
 
-    // Определить функцию обратного вызова для обработки изменений в медиа-запросе
+    // Define a callback function to handle changes to the media query
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
     };
 
-    // Добавдляем функцию обратного вызова в качестве прослушивателя для изменений в медиа-запросе
+    // Add the callback function as a listener for changes to the media query
     mediaQuery.addEventListener("change", handleMediaQueryChange);
 
-    // Удаляем прослушиватель, когда компонент отмонтирован
+    // Remove the listener when the component is unmounted
     return () => {
       mediaQuery.removeEventListener("change", handleMediaQueryChange);
     };
@@ -55,8 +55,9 @@ const ComputersCanvas = () => {
 
   return (
     <Canvas
-      frameloop="demand"
+      frameloop='demand'
       shadows
+      dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
@@ -71,7 +72,7 @@ const ComputersCanvas = () => {
 
       <Preload all />
     </Canvas>
-  )
+  );
 };
 
 export default ComputersCanvas;
